dfad6507fe0943c4603a8fcd1d8898460541c95b
Edu1Sinico <EduardoSenai888@gmail.com>
2024-03-21 20:51:52 -0300

    Enviando o bglh

--- Aula_03_Python/desafioManipulacao.py
+++ Aula_03_Python/desafioManipulacao.py
@@ -1,4 +1,4 @@
-# Desafio 01
+# Desafio 01:
 nomeCompleto = (input("Digite o seu nome completo: "))
 
 arrayNome = nomeCompleto.split()
@@ -8,10 +8,32 @@ print(f"Seu nome com todas as letras minúsculas: {nomeCompleto.lower()}\n")
 print(f"Quantidade de letras em seu nome: {len(nomeCompleto.replace(" ",""))}\n")
 print(f"Quantidade de letras no seu primeiro nome: {len(arrayNome[0])}\n")
 
-# Desafio 02
+# Desafio 02:
 valor = (input("Digite um valor entre 0 e 9999: "))
+uMedida = ["Unidade","Dezena","Centena","Milhar"]
 
-print(f"Unidade: {valor[0]}")
-print(f"Dezena: {valor[1]}")
-print(f"Centena: {valor[2]}")
-print(f"Milhar: {valor[3]}")
+for i in range(4):
+    print(uMedida[i] + ": " + valor[i])
+
+# Desafio 03:
+cidade = (input("Digite uma cidade: "))
+arrayCidade = cidade.split()
+
+if arrayCidade[0] == "Santo":
+    print(f"A cidade {cidade} possuí 'Santo' no seu primeiro nome")
+else:
+    print(f"A cidade {cidade} não possuí 'Santo' no seu primeiro nome")
+
+# Desafio 04
+nome = (input("Digite o seu nome completo: "))
+
+if "Silva" in nome:
+    print("Seu nome possuí 'Silva'.")
+else:
+    print("Seu nome não possuí 'Silva'.")
+
+# Desafio 05
+frase = (input("Digite a Frase: "))
+
+print(f"A quarta palavra é: {frase.count("o")}")
+
