ccd84948c63ef201a85bbf707c4c8eb37171baea
Edu Sinico <123770640+Edu1Sinico@users.noreply.github.com>
2024-03-28 18:34:14 -0300

    Update desafioManipulacao.py

--- Aula_03_Python/desafioManipulacao.py
+++ Aula_03_Python/desafioManipulacao.py
@@ -33,7 +33,32 @@ else:
     print("Seu nome não possuí 'Silva'.")
 
 # Desafio 05
-frase = (input("Digite a Frase: "))
+frase = input("Digite uma frase: ")
 
-print(f"A quarta palavra é: {frase.count("o")}")
+contador_A = 0
+primeira_posicao = None
+ultima_posicao = None
 
+for i in range(len(frase)):
+    if frase[i] == 'A' or frase[i] == 'a':
+        contador_A += 1
+        if primeira_posicao is None: 
+            primeira_posicao = i + 1 
+        ultima_posicao = i + 1
+
+print("Quantidade de vezes que 'A' aparece:", contador_A)
+if contador_A > 0:
+    print("Posição da primeira ocorrência de 'A':", primeira_posicao)
+    print("Posição da última ocorrência de 'A':", ultima_posicao)
+else:
+    print("A letra 'A' não foi encontrada na frase.")
+
+# Desafio 06
+nome_completo = input("Digite o nome completo: ")
+
+partes_nome = nome_completo.split()
+primeiro_nome = partes_nome[0]
+ultimo_nome = partes_nome[-1]
+
+print("Primeiro nome:", primeiro_nome)
+print("Último nome:", ultimo_nome)
