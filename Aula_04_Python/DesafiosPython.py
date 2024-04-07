f17f583d44a8e682adae1e23ae926085b6741952
Edu1Sinico <EduardoSenai888@gmail.com>
2024-04-07 15:41:52 -0300

    Enviando os desafios prontos de python para o github

--- Aula_04_Python/DesafiosPython.py
+++ Aula_04_Python/DesafiosPython.py
@@ -48,4 +48,48 @@ else:
 
 # Exercício 5
 print("\n-----------------------------------------------\nExercício 5")
+ano = int(input("Digite um ano qualquer: "))
 
+if ano%4 == 0:
+    print("\nO ano digitado é bissexto")
+else:
+    print("\nano digitado não é bissexto")
+
+# Exercício 6
+print("\n-----------------------------------------------\nExercício 6")
+num1 = int(input("Informe o primeiro número: "))
+num2 = int(input("\nInforme o segundo número: "))
+num3 = int(input("\nInforme o terceiro número: "))
+
+if num1 > num2 and num1 > num3:
+    resultado = "\nO primeiro número digitado é o maior."
+elif num2 > num1 and num2 > num3:
+    resultado = "\nO segundo número digitado é o maior."
+elif num3 > num1 and num3 > num2:
+    resultado = "\nO terceiro número digitado é o maior."
+else:
+    resultado = "\n Alguns dos números são iguais."
+
+print(resultado)
+
+# Exercício 7
+print("\n-----------------------------------------------\nExercício 7")
+salario = float(input("Digite o seu salário: R$ "))
+
+if salario > 1250:
+    salarioAumentado = salario + (salario*0.1)
+else:
+    salarioAumentado = salario + (salario*0.15)
+
+print(f"\nO salário foi aumentado para: R$ {salarioAumentado}")
+
+# Exercício 8
+print("\n-----------------------------------------------\nExercício 8")
+a = int(input("Informe o primeiro comprimento: "))
+b = int(input("\nInforme o segundo comprimento: "))
+c = int(input("\nInforme o terceiro comprimento: "))
+
+if (a+b>c) and (a+c>b) and (b+c>a):
+    print("\nOs valores dos lados forma um triângulo.\n")
+else:
+    print("\nOs valores dos lados não forma um triângulo\n")
